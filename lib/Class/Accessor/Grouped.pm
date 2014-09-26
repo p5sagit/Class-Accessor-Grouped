5802bd3e7c055b942623d3bbbc410bc45559c9c9
Peter Rabbitson <ribasushi@cpan.org>
2014-09-26 15:25:31 +0200

    Release 0.10011

--- lib/Class/Accessor/Grouped.pm
+++ lib/Class/Accessor/Grouped.pm
@@ -15,7 +15,7 @@ BEGIN {
   }
 }
 
-our $VERSION = '0.10010';
+our $VERSION = '0.10011';
 $VERSION = eval $VERSION if $VERSION =~ /_/; # numify for warning-free dev releases
 
 # when changing minimum version don't forget to adjust Makefile.PL as well
